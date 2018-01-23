8c83edd5395ae7cdf01da2fb85a037c478459387
TaiYi <931305033@qq.com>
2018-01-23 17:36:23 +0800

    fix method

--- SETCONFIG/CONNECTIONSET.h
+++ SETCONFIG/CONNECTIONSET.h
@@ -10,7 +10,14 @@
 #include <string.h>
 #include <pthread.h>
 
+typedef struct _SOCKET_INFORMATION   {
+    char name[32];        // 发送和接收数据的缓冲区
+    int Socket;                      // 与客户端进行通信的套接字
+} SOCKET_INFORMATION;
+
+
 #define BUFFER_SIZE 256 // 这是接收缓冲区的大小， 发送缓冲区动态指定，动态分配
 #define QUEUE 10        // 设置队列
+#define INDEX 1000
 
 #endif
--- main.c
+++ main.c
@@ -2,16 +2,26 @@
 #include "../SETCONFIG/CONNECTIONSET.h"
 #include "DATABASES/CONNECTDATABASE.h"
 
-LOGIN_INFO * get_login_info(char json[]);
-void test2();
-void test3();
+int TotalSockets = 0;
+SOCKET_INFORMATION SOCKER_ARRAY[INDEX];
+
+//发送好友信息到客户端
+void send_friend_list_to_client(int server_fd);
+//获取登陆信息
+LOGIN_INFO* get_login_info(char json[]);
+//获取登出信息
+LOGOUT_INFO* get_logout_info(char json[]);
+
+CHAT_INFO* get_chat_info(char json[]);
+
 void test4();
-void re_test();
 
-void test();
+//记录登陆信息
+void creater_SOCKER_ARRAY_index(int socket,char* name);
+//登出，并关闭socket
+void delete_SOCKER_ARRA_index(int index);
 
-void send_friend_list_to_client();
-
+//初始化服务器连接
 int initServer() {
     //声明server句柄，setsockopt的开关量
     int server_fd,on=1;
@@ -43,12 +53,10 @@ int initServer() {
     return server_fd;
 }
 
+
 void thread(void* args) {
     //客户机传上来的数据
     char buffer_client[BUFFER_SIZE];
-
-    //服务器将要发出的数据
-    //char buffer_server[BUFFER_SIZE];
 
     //客户机的连接句柄
     int client_sock_fd = *(int *) args;
@@ -64,51 +72,84 @@ void thread(void* args) {
     //获取可用字符串
     int size = read(client_sock_fd, buffer_client, BUFFER_SIZE);
 
-//  判定是否断开连接
-//  if (buffer_client[0] == 'e' && buffer_client[1] == 'x' && buffer_client[2] == 'i' && buffer_client[3] == 't'){
-//      break;
-//  }
-    buffer_client[size] = "\0";
-
     //初次判断接收的请求Action是什么
-    CHAT_INFO* info;
+    CHAT_INFO* info = malloc(sizeof(CHAT_INFO));
     //为info赋值
     getAction(info,buffer_client);
 
     //内存占用8个字节
     LOGIN_INFO* login_info;
+    LOGOUT_INFO* logout_info;
+    CHAT_INFO* chat_info;
 
-    switch (info->ACTION){
+    switch (info->ACTION) {
         case 0:
             login_info = get_login_info(buffer_client);
 
             //解析后查数据库，登陆成功反回状态信息
-            if (1){
+            if (1) {
                 RE_INFO r;
                 r.STATUS = 7;
                 r.t = "2018-01-01";
                 char *pJ = re_status_info(&r);
-                write(client_sock_fd, pJ, strlen(pJ));
+                int len = strlen(pJ);
+                write(client_sock_fd, pJ, len);
+                write(client_sock_fd, "\n", sizeof("\n"));
 
-                free(login_info);
-
+                char ID[16] = "931305033";
+                creater_SOCKER_ARRAY_index(client_sock_fd, ID);
                 //主动发出信息
-                //send_friend_list_to_client(login_info->IP);
-            } else{
+                send_friend_list_to_client(client_sock_fd);
+            } else {
                 RE_INFO r;
                 r.STATUS = 444;//登陆失败，账号或密码错误
                 r.t = "2018-01-01";
                 char *pJ = re_status_info(&r);
                 write(client_sock_fd, pJ, strlen(pJ));
             }
-
             break;
 
         case 886:
+            logout_info = get_logout_info(buffer_client);
+            //解析后查数据库，登陆成功反回状态信息
+            if (1) {
+                RE_INFO r;
+                r.STATUS = 7;
+                r.t = "2018-01-01";
+                char *pJ = re_status_info(&r);
+                write(client_sock_fd, pJ, strlen(pJ));
+                write(client_sock_fd, "\n", sizeof("\n"));
 
+                for (int i = 0; i < TotalSockets; ++i) {
+                    if (!strcmp(SOCKER_ARRAY[i].name, logout_info->account)) {
+                        delete_SOCKER_ARRA_index(i);
+                    }
+
+                }
+
+            } else {
+                RE_INFO r;
+                r.STATUS = 444;//登陆失败，账号或密码错误
+                r.t = "2018-01-01";
+                char *pJ = re_status_info(&r);
+                write(client_sock_fd, pJ, strlen(pJ));
+            }
             break;
 
         case 11:
+            chat_info = get_chat_info(buffer_client);
+
+            for (int i = 0; i < TotalSockets; ++i) {
+
+                if(!strcmp(SOCKER_ARRAY[i].name,chat_info->account)){
+                    
+                    printf("%s++++++%s\n",SOCKER_ARRAY[i].name,chat_info->account);
+
+                    write(client_sock_fd, "ffff", strlen("ffff"));
+                    write(client_sock_fd, "\n", sizeof("\n"));
+                }
+
+            }
 
             break;
 
@@ -119,30 +160,31 @@ void thread(void* args) {
             break;
     }
 
-    close(client_sock_fd);
-
-
     pthread_exit(0);
 }
 
-void send_friend_list_to_client() {
-    int server_fd,on=1;
-    struct sockaddr_in *server_add;
-    server_add = malloc(sizeof(struct sockaddr_in));
+int main() {
+    int server_fd = initServer();
+    struct sockaddr_in *client_addr = malloc(sizeof(struct sockaddr_in));
+    int len = sizeof(client_addr);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    server_add->sin_addr.s_addr = htonl(INADDR_ANY);
-    server_add->sin_family = AF_INET;
-    server_add->sin_port = htons(6666);
+    while (666){
+        pthread_t thread_id;
+        //阻塞，直到有用户连接
+        int *client_sockfd = (int *) malloc(sizeof(int));
+        *client_sockfd = accept(server_fd, client_addr, &len);
 
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(int));
-
-    if (connect(server_fd, server_add, sizeof(struct sockaddr_in)) == -1) {
-        perror("connect error");
-        exit(-1);
+        //在顺序执行不循环的状态下，顺序执行，主线程执行完直接结束，可能不执行thread
+        pthread_create(&thread_id,NULL,thread,client_sockfd);
+        free(client_sockfd);
     }
 
+    close(server_fd);
 
+    return EXIT_SUCCESS;
+}
+
+void send_friend_list_to_client(int server_fd) {
     FRIEND aFriend[3];
     strcpy(aFriend[0].u_name, "eddie");
     strcpy(aFriend[1].u_name, "alice");
@@ -153,61 +195,11 @@ void send_friend_list_to_client() {
     strcpy(aFriend[2].IP, "127.0.0.1");
     char *pJson = re_friend_info(&aFriend, 3);
     write(server_fd, pJson, strlen(pJson));
+    write(server_fd, "\n", sizeof("\n"));
 
-}
-
-void connect_client() {
-    int server_fd = initServer();
-    struct sockaddr_in client_addr;
-    int len = sizeof(client_addr);
-    while (666){
-        pthread_t thread_id;
-        //阻塞，直到有用户连接
-        int *client_sockfd = (int *) malloc(sizeof(int));
-        *client_sockfd = accept(server_fd, &client_addr, &len);
-
-        //在顺序执行不循环的状态下，顺序执行，主线程执行完直接结束，可能不执行thread
-        pthread_create(&thread_id,NULL,thread,client_sockfd);
-        free(client_sockfd);
-    }
-    close(server_fd);
-}
-
-void process_test() {
-    printf("start...\n");
-    fflush(stdout);
-    int mFork = fork();
-    //进程，复制内存（context），做轮寻 分配时间片
-    if (mFork > 0) {
-        printf("%d\n\n",mFork);
-        for (int i = 0; i < 100; ++i) {
-            printf("main process ...%d\n", i);
-        }
-    } else{
-        printf("%d\n\n\n",mFork);
-        for (int i = 0; i < 100; ++i) {
-            printf("\tsub process ...%d\n", i);
-        }
-    }
-}
-
-int main() {
-    connect_client();
-
-    return EXIT_SUCCESS;
-}
-
-void test() {
-    printf("----------------------------------\n");
-    //test1();
-    test2();
-    test3();
-    test4();
-    re_test();
 }
 
 LOGIN_INFO * get_login_info(char json[]) {
-    printf("Login_info : \n");
     LOGIN_INFO *login_info = malloc(sizeof(LOGIN_INFO));
     send_client_login_info(login_info,json);
 
@@ -217,31 +209,19 @@ LOGIN_INFO * get_login_info(char json[]) {
 }
 
 
-void test2() {
-    printf("Logout_info : \n");
+LOGOUT_INFO * get_logout_info(char json[]) {
     LOGOUT_INFO *logout_info = malloc(sizeof(LOGOUT_INFO));
-    char json[] = "{\"IP\":\"127.0.0.1\",\"ACTION\":886,\"account\":\"931305033\",\"time\":\"2018-01-01\"}";
     send_client_logout_info(logout_info,json);
 
-    printf("IP : %s\n",logout_info->IP);
-    printf("ACTION : %d\n",logout_info->ACTION);
-    printf("account : %s\n",logout_info->account);
-    printf("TIME : %s\n",logout_info->t);
-    free(logout_info);
-    printf("----------------------------------\n");
+    return logout_info;
 }
 
-void test3() {
-    printf("Chat_info : \n");
+CHAT_INFO* get_chat_info(char json[]) {
     CHAT_INFO * chat_info = malloc(sizeof(CHAT_INFO));
-    char json[] = "{\"IP\":\"127.0.0.1\",\"ACTION\":11,\"account\":\"931305033\",\"time\":\"2018-01-01\"}";
     send_client_chat_info(chat_info,json);
-    printf("IP : %s\n",chat_info->IP);
-    printf("ACTION : %d\n",chat_info->ACTION);
-    printf("account : %s\n",chat_info->account);
-    printf("TIME : %s\n",chat_info->t);
     free(chat_info);
-    printf("----------------------------------\n");
+
+    return chat_info;
 }
 
 
@@ -257,30 +237,27 @@ void test4() {
     printf("----------------------------------\n");
 }
 
-void re_test() {
-
-    RE_INFO r;
-    r.STATUS = 7;
-    r.t = "2018-01-01";
-
-    FRIEND aFriend[3];
-    strcpy(aFriend[0].u_name, "eddie");
-    strcpy(aFriend[1].u_name, "alice");
-    strcpy(aFriend[2].u_name, "jim");
-
-    strcpy(aFriend[0].IP, "127.0.0.1");
-    strcpy(aFriend[1].IP, "127.0.0.1");
-    strcpy(aFriend[2].IP, "127.0.0.1");
-
-    char *pJson = re_friend_info(&aFriend, 3);
-    printf("%s\n", pJson);
-
-    printf("----------------------------------\n");
-
-    char *pJ = re_status_info(&r);
-    printf("%s\n", pJ);
-
-    printf("----------------------------------\n");
+void creater_SOCKER_ARRAY_index(int socket,char ID[32]){
+    if (TotalSockets<1000){
+        SOCKET_INFORMATION SI;
+        strcpy(SI.name,ID);
+        SI.Socket = socket;
+        SOCKER_ARRAY[TotalSockets] = SI;
+        printf("%s\n",SI.name);
+        TotalSockets++;
+    } else {
+        //如果超出连接池大小
+        return;
+    }
 }
 
-
+void delete_SOCKER_ARRA_index(int index){
+    close(SOCKER_ARRAY[index].Socket);
+    if (TotalSockets >= 0){
+        for (int i = index; i <= TotalSockets; ++i) {
+            SOCKER_ARRAY[index] = SOCKER_ARRAY[index+1];
+        }
+    } else{
+        return;
+    }
+};
